651c800b5ac98c214894571cb490ad69424bdc79
cha0sg0d <0xcha0sg0d@gmail.com>
2022-08-25 13:57:08 -0700

    placeholder for Mint NFT component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,11 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { RoundList } from "./components/RoundList";
 import { NewRoundForm } from "./components/NewRoundForm";
 import { AdminManager } from "./components/AdminManager";
+import { useEffect, useState } from "react";
 
-function App() {
+function NewGrandPrix() {
   return (
-    <Container>
-      <Nav>
-        <Logo>Dynasty Admin</Logo>
-        <ConnectButton />
-      </Nav>
+    <>
       <Title>Create new Round</Title>
       <NewRoundForm />
       <div style={{ height: "2rem" }} />
@@ -20,6 +17,35 @@ function App() {
       <div style={{ height: "2rem" }} />
       <Title>Manage Admins</Title>
       <AdminManager />
+    </>
+  );
+}
+
+function MintNFT() {
+  return (
+    <>
+      <Title>Mint Reward</Title>
+    </>
+  );
+}
+
+export const Body: React.FC<{ mint: boolean }> = ({ mint }) => {
+  return mint ? <MintNFT /> : <NewGrandPrix />;
+};
+
+function App() {
+  const [mint, setMint] = useState(false);
+
+  return (
+    <Container>
+      <Nav>
+        <Logo>Dynasty Admin</Logo>
+        <ConnectButton />
+      </Nav>
+      <Button onClick={() => setMint(!mint)}>
+        {mint ? "Create Round" : "Mint NFT"}
+      </Button>
+      <Body mint={mint} />
     </Container>
   );
 }
@@ -30,6 +56,13 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
+  margin: 1rem;
+`;
+
+const Button = styled.button`
+  flex-direction: column;
+  align-items: center;
+  font-family: "Menlo", "Inconsolata", monospace;
   margin: 1rem;
 `;
 
